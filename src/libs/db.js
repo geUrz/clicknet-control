679ec4fbf56fe51da40b8fc0e50af47851ec034d
geUrz <gerardourzua20@gmail.com>
2024-09-23 12:47:45 -0700

    start7

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,18 +1,19 @@
-import mysql from 'mysql2/promise'
+import mysql from 'mysql2/promise';
 
-// Configura la conexión a la base de datos
+// Configura la conexión a la base de datos utilizando las variables de entorno
 const connection = mysql.createPool({
-  host: 'autorack.proxy.rlwy.net',   
-  port: 14849,   
-  user: 'root',           
-  password: 'TJiPOtvOrgKhbNISFcuKTbJekryFSJIQ',    
-  database: 'railway',  
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-})
+});
 
-export default connection   
+export default connection;
+
 
 /* import mysql from 'mysql2/promise';
 
