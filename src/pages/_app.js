8f3a2aa872e97f7ca3172e33948b32ee017249c3
geUrz <gerardourzua20@gmail.com>
2024-09-24 13:46:37 -0700

    start16

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,17 @@
 
 import 'semantic-ui-css/semantic.min.css'
 import { initializeOneSignal } from '@/libs/onesignal'
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 import '@/styles/globals.css'
-import { AuthProvider } from '@/contexts/AuthContext';
+import { AuthProvider } from '@/contexts/AuthContext'
 
 export default function App(props) {
 
   const { Component, pageProps } = props
 
-  /* useEffect(() => {
+  useEffect(() => {
     initializeOneSignal()
-  }, []) */
+  }, []) 
 
   return(
   
