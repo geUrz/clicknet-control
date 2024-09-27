9005fa0ae72382607ac5118cc1727f7f63317e3f
geUrz <gerardourzua20@gmail.com>
2024-09-26 17:07:31 -0700

    start35

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,31 @@
+import 'semantic-ui-css/semantic.min.css';
+import { initializeOneSignal } from '@/libs/onesignal';
+import { useEffect } from 'react';
+import '@/styles/globals.css';
+import { AuthProvider } from '@/contexts/AuthContext';
 
-import 'semantic-ui-css/semantic.min.css'
-import { initializeOneSignal } from '@/libs/onesignal'
-import { useEffect } from 'react'
-import '@/styles/globals.css'
-import { AuthProvider } from '@/contexts/AuthContext'
+const getCookie = (name) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+};
 
 export default function App(props) {
-
-  const { Component, pageProps } = props
+  const { Component, pageProps } = props;
 
   useEffect(() => {
-    initializeOneSignal()
-  }, []) 
+    const userId = getCookie('userId'); // Obtener el userId de las cookies
+    
+    if (userId) {
+      initializeOneSignal(); // Inicializar OneSignal solo si el userId está disponible
+    } else {
+      console.log('User ID no disponible. Asegúrate de hacer login primero.');
+    }
+  }, []); // Ejecutar solo una vez al montar el componente
 
-  return(
-  
+  return (
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
-
-  ) 
+  );
 }
