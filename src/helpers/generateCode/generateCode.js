34de391ce454824b2fe6fb892304d1adc2acfb25
geUrz <gerardourzua20@gmail.com>
2024-10-02 21:32:04 -0700

    start43

--- src/helpers/generateCode/generateCode.js
+++ src/helpers/generateCode/generateCode.js
@@ -1,6 +1,4 @@
 import QRCode from 'qrcode'
-import Jimp from "jimp";
-import path from 'path'
 
 export function generateCode(length = 10) {
   const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -23,29 +21,4 @@ export async function generateQR(code) {
   }
 }
 
-export async function generateQRCodeWithLogo(code) {
-  // Genera el código QR en formato buffer
-  const qrCodeBuffer = await QRCode.toBuffer(code, {
-    errorCorrectionLevel: 'H',
-    color: {
-      dark: '#010599FF',
-      light: '#FFBF60FF',
-    },
-  });
-
-  // Carga la imagen QR generada
-  const qrImage = await Jimp.read(qrCodeBuffer);
-
-  // Cargar el logo desde una URL o ruta
-  const logo = await Jimp.read('public/img/logoqr.png');
-
-  // Redimensionar el logo y colocarlo en el centro del QR
-  logo.resize(qrImage.bitmap.width / 4, Jimp.AUTO);
-  const x = (qrImage.bitmap.width / 2) - (logo.bitmap.width / 2);
-  const y = (qrImage.bitmap.height / 2) - (logo.bitmap.height / 2);
-  qrImage.composite(logo, x, y);
-
-  // Convertir la imagen final del QR con logo a base64
-  return await qrImage.getBase64Async(Jimp.MIME_PNG);
-}
 
