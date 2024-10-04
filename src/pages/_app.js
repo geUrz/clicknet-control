fe2ce0e3567f05cc5ce44b78c05069ee25af9073
geUrz <gerardourzua20@gmail.com>
2024-10-03 22:24:14 -0700

    start55

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,17 @@
 import 'semantic-ui-css/semantic.min.css';
-import { initializeOneSignal } from '@/libs/onesignal';
-import { useEffect } from 'react';
 import '@/styles/globals.css';
 import { AuthProvider } from '@/contexts/AuthContext';
 
-const getCookie = (name) => {
+/* const getCookie = (name) => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
-}
+} */
 
 export default function App(props) {
   const { Component, pageProps } = props;
 
-  useEffect(() => {
+  /* useEffect(() => {
     const userId = getCookie('userId')
     
     if (userId) {
@@ -21,7 +19,7 @@ export default function App(props) {
     } else {
       console.log('User ID no disponible. Asegúrate de hacer login primero.');
     }
-  }, [])
+  }, []) */
 
   return (
     <AuthProvider>
